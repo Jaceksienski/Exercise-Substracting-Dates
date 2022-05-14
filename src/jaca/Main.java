b03b5d6b343993c48f407694ebff4e6996797595
jaceksienski <jacek.sienski.jr@gmial.com>
2022-05-14 19:47:07 +0200

    Rewritten and working exercise.

--- src/jaca/Main.java
+++ src/jaca/Main.java
@@ -1,77 +1,50 @@
 package jaca;
 
-import java.sql.PreparedStatement;
-import java.util.Date;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
-//12:30pm-12:00am
+import static java.time.temporal.ChronoUnit.MINUTES;
+
+//input:    12:30pm-12:00am
+//output:   690
 
 public class Main {
 
     public static String StringChallenge(String str) {
 
-        Date date = new Date();
-        System.out.println(date.getTime());
+        String[] datesWithAmPm = str.split("-");
+        boolean[] isPm = new boolean[2];
 
-        String date1Hours = str.substring(0, 2);
-        String date1Minutes = str.substring(3, 5);
-        String date1AmOrPm = str.substring(5, 6);
+        if (datesWithAmPm[0].contains("pm")) isPm[0] = true;
+        if (datesWithAmPm[1].contains("pm")) isPm[1] = true;
 
-        boolean isMoved = false;
-        try {
-            Double.parseDouble(date1Hours);
-        } catch(NumberFormatException e){
-            date1Hours = str.substring(0, 1);
-            date1Minutes = str.substring(2, 4);
-            date1AmOrPm = str.substring(4, 5);
+        datesWithAmPm[0] = datesWithAmPm[0].substring(0, datesWithAmPm[0].length() - 2);
+        datesWithAmPm[1] = datesWithAmPm[1].substring(0, datesWithAmPm[1].length() - 2);
+
+        ArrayList<String> dates = new ArrayList();
+        ArrayList<Integer> datesInt = new ArrayList();
+        dates.addAll(List.of(datesWithAmPm[0].split(":")));
+        dates.addAll(List.of(datesWithAmPm[1].split(":")));
+
+        for (String s:
+             dates) {
+            datesInt.add(Integer.valueOf(s));
+        }
+        if(isPm[0]) {
+            datesInt.set(0, datesInt.get(0) + 12);
+            if (datesInt.get(0) == 24) datesInt.set(0,0);
+        }
+        if(isPm[1]) {
+            datesInt.set(2, datesInt.get(2)+12);
+            if (datesInt.get(2) == 24) datesInt.set(2,0);
         }
 
-        int date1minInt = Integer.parseInt(date1Minutes);
-        boolean isDate1Am = date1AmOrPm.equals("a");
+        LocalTime time1 = LocalTime.of(datesInt.get(0),datesInt.get(1));
+        LocalTime time2 = LocalTime.of(datesInt.get(2),datesInt.get(3));
 
-        int date1H = Integer.parseInt(date1Hours);
-        if (!isDate1Am) date1H = date1H +12;
-
-
-        String date2Hours = str.substring(8, 10);
-        String date2Minutes = str.substring(11, 13);
-        String date2AmOrPm = str.substring(13, 14);
-        if(isMoved){
-            date2Hours = str.substring(7, 9);
-            date2Minutes = str.substring(10, 12);
-            date2AmOrPm = str.substring(12, 13);
-
-        }
-        try {
-            Double.parseDouble(date1Hours);
-        } catch(NumberFormatException e){
-            date2Hours = str.substring(8, 9);
-            date2Minutes = str.substring(10, 12);
-            date2AmOrPm = str.substring(12, 13);
-            if(isMoved){
-                date2Hours = str.substring(7, 8);
-                date2Minutes = str.substring(9, 11);
-                date2AmOrPm = str.substring(11, 12);
-
-            }
-        }
-        if(isMoved){
-
-
-        }
-        int date2minInt = Integer.parseInt(date2Minutes);
-
-        boolean isDate2Am = date2AmOrPm.equals("a");
-
-        int date2H = Integer.parseInt(date2Hours);
-        if (!isDate2Am) date2H = date2H +12;
-
-
-        int result = (date1H - date2H) *60;
-        result = result - date1minInt - date2minInt;
-
-
-        str = String.valueOf(result);
+        str = String.valueOf(MINUTES.between(time1, time2));
 
         return str;
     }
